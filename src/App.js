2a1849b2a1a71942b82d5a07a7c44cc8b32810f9
prakasiwi <muhammadgalih282@gmail.com>
2021-01-05 15:53:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';  
+import './App.css';  
+import Alert from './Components/Alert'
+import Media from './Components/Media'
+  
+class App extends Component {  
+  render(){  
+    return (  
+      <div className="App container">  
+        <div className="alert alert-info">  
+          <h3 className="judul">Ini Project Pertama React JS</h3>  
+          <p className="subjudul">Belajar React JS itu mudah</p>  
+          <button className="mr-1 btn btn-success">Setuju</button>  
+          <button className="btn btn-info">Iya Dong</button>  
+        </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <div className="App container col-sm-6">  
+          <Alert type="danger" header="Fatal Error">  
+            Ini adalah alert dengan tipe danger  
+          </Alert>  
+          <Alert type="success" header="Berhasil">  
+            Ini adalah alert dengan tipe success  
+          </Alert>  
+          <Alert type="warning" header="Warning">  
+            Ini adalah alert dengan tipe warning  
+          </Alert>  
+        </div>
 
-export default App;
+        <Media image="mobil1.jpg" title="Honda">
+          Ini adalah mobil
+        </Media> 
+        <Media image="mobil2.jpg" title="Honda">
+          Ini juga mobil
+        </Media>
+      </div>
+        
+    );  
+  }  
+}  
+export default App;  
